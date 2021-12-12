27e7d400aacadbe3eeee0f24a701f5df98c053c1
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2021-12-12 17:14:25 +0100

    Add custom persistence layer

--- bin/server.js
+++ bin/server.js
@@ -6,7 +6,7 @@
 const WebSocket = require('ws')
 const http = require('http')
 const wss = new WebSocket.Server({ noServer: true })
-const setupWSConnection = require('./utils.js').setupWSConnection
+const utils = require('./utils.js')
 
 const host = process.env.HOST || 'localhost'
 const port = process.env.PORT || 1234
@@ -16,7 +16,27 @@ const server = http.createServer((request, response) => {
   response.end('okay')
 })
 
-wss.on('connection', setupWSConnection)
+utils.setPersistence({
+  bindState: (documentName, doc) => {
+    // Here you listen to granular document updates and store them in the database
+    // You don't have to do this, but it ensures that you don't lose content when the server crashes
+    // See https://github.com/yjs/yjs#Document-Updates for documentation on how to encode
+    // document updates
+    console.log('Custom persistence bind state')
+  },
+  writeState: (string, doc) => {
+    // This is called when all connections to the document are closed.
+    // In the future, this method might also be called in intervals or after a certain number of updates.
+    console.log('Custom persistence writeState')
+    return new Promise(resolve => {
+      // When the returned Promise resolves, the document will be destroyed.
+      // So make sure that the document really has been written to the database.
+      resolve()
+    })
+  }
+})
+
+wss.on('connection', utils.setupWSConnection)
 
 server.on('upgrade', (request, socket, head) => {
   // You may check auth of request here..
